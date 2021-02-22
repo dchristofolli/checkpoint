b463854f8bdff8e042d9e14a4db923abd8d1025a
Daniel Christofolli <danielchristofolli@gmail.com>
2021-02-21 22:03:24 -0300

    Validação de dias da semana e intervalos implementada

--- src/main/java/com/dchristofolli/checkpoint/v1/service/TimeService.java
+++ src/main/java/com/dchristofolli/checkpoint/v1/service/TimeService.java
@@ -2,23 +2,29 @@ package com.dchristofolli.checkpoint.v1.service;
 
 import com.dchristofolli.checkpoint.domain.model.TimeRegistrationEntity;
 import com.dchristofolli.checkpoint.domain.repository.TimeRegistrationRepository;
+import com.dchristofolli.checkpoint.exception.ApiException;
 import com.dchristofolli.checkpoint.v1.dto.TimeRegistrationRequestDto;
 import com.dchristofolli.checkpoint.v1.dto.TimeRegistrationResponseDto;
+import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.DateTimeException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.dchristofolli.checkpoint.v1.mapper.TimeRegistrationMapper.mapToResponse;
 
 @Service
 public class TimeService {
     private final TimeRegistrationRepository timeRegistrationRepository;
+    private static final DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd-MM-yyyy");
     private final Logger log = LoggerFactory.getLogger("Time Service");
 
     public TimeService(TimeRegistrationRepository timeRegistrationRepository) {
@@ -26,9 +32,9 @@ public class TimeService {
     }
 
     public TimeRegistrationResponseDto timeRegistration(TimeRegistrationRequestDto dto) {
+        weekDayValidator(dto);
         TimeRegistrationEntity entity;
-        DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd-MM-yyyy");
-        departureBeforeEntry(dto);
+        intervalLessThanAnHour(dto);
         if (dto.getTime() == null) {
             entity = actualDateTimeRegistration(dto);
         } else
@@ -40,6 +46,7 @@ public class TimeService {
     }
 
     private TimeRegistrationEntity actualDateTimeRegistration(TimeRegistrationRequestDto dto) {
+        getLastRecordByEmployeeAndDate(dto);
         TimeRegistrationEntity entity;
         LocalDate today = LocalDate.now();
         LocalTime nowTime = LocalTime.now();
@@ -50,16 +57,37 @@ public class TimeService {
         return entity;
     }
 
-    private void departureBeforeEntry(TimeRegistrationRequestDto dto) {
-        DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd-MM-yyyy");
-        boolean isBefore = timeRegistrationRepository
+    private void intervalLessThanAnHour(TimeRegistrationRequestDto dto) {
+        LocalTime lastRecord = getLastRecordByEmployeeAndDate(dto);
+        boolean isBefore = getAllRegistriesByEmployeeAndDate(dto)
+            .parallelStream()
+            .anyMatch(time -> LocalTime.parse(dto.getTime()).isBefore(time.plusHours(1)));
+        if (isBefore)
+            throw new DateTimeException("Invalid record, the last record was made at " +
+                lastRecord.toString().substring(0, 5) +
+                ". Please, insert a time after " + lastRecord.plusHours(1).toString().substring(0, 5));
+    }
+
+    private LocalTime getLastRecordByEmployeeAndDate(TimeRegistrationRequestDto dto) {
+        return getAllRegistriesByEmployeeAndDate(dto)
+            .parallelStream()
+            .max(LocalTime::compareTo)
+            .orElseGet(() -> LocalTime.now().minusHours(8));
+    }
+
+    private List<LocalTime> getAllRegistriesByEmployeeAndDate(TimeRegistrationRequestDto dto) {
+        return timeRegistrationRepository
             .findAllByEmployeeCpfAndAndDate(
                 dto.getEmployeeCpf(),
                 LocalDate.parse(dto.getDate(), dateFormat).toString())
-            .stream()
-            .anyMatch(entity -> (LocalTime.parse(dto.getTime())).isBefore(LocalTime.parse(entity.getTime())));
-        if (isBefore)
-            throw new DateTimeException("Invalid record, the last record was made at {}" +
-                ". Please, insert a time after {}");
+            .parallelStream()
+            .map(entity -> LocalTime.parse(entity.getTime()))
+            .collect(Collectors.toList());
+    }
+
+    private void weekDayValidator(TimeRegistrationRequestDto dto) {
+        String weekday = LocalDate.parse(dto.getDate(), dateFormat).dayOfWeek().getAsText();
+        if (weekday.equals("Saturday") || weekday.equals("Sunday"))
+            throw new ApiException("You can't work at weekends. Have fun :)", HttpStatus.BAD_REQUEST);
     }
 }
